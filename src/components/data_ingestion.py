ff5b2fb1a9454df962b11abdbb1fcb16e7a77c70
ShefaliShettyy <shefalishettyy@gmail.com>
2023-09-12 19:45:08 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,8 +6,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
-@dataclass
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
+@dataclass   #input thing required for the data transformation
 class DataIngestionConfig: #these are the inputs given to data ingestion comp
     train_data_path: str = os.path.join('artifacts', "train.csv")
     test_data_path: str = os.path.join('artifacts', "test.csv")
@@ -46,9 +52,17 @@ class DataIngestion:
             raise CustomException(e, sys)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #combines data ingestion and tranformation
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+
+
 
 
 
